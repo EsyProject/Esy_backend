60fef371b801453231b76579557065b2b868779a
BrnGomes0 <brunownog05@gmail.com>
2024-04-30 07:44:18 -0300

    :sparkles: feat: creating a feat email

--- src/main/java/apiBoschEsy/apiInSpringBoot/controller/EmailController.java
+++ src/main/java/apiBoschEsy/apiInSpringBoot/controller/EmailController.java
@@ -1,21 +1,9 @@
 package apiBoschEsy.apiInSpringBoot.controller;
 
-import apiBoschEsy.apiInSpringBoot.dto.auth.DataAuth;
-import apiBoschEsy.apiInSpringBoot.dto.email.DataDetailEmail;
-import apiBoschEsy.apiInSpringBoot.dto.email.DataRegisterEmail;
-import apiBoschEsy.apiInSpringBoot.entity.Email;
 import apiBoschEsy.apiInSpringBoot.service.email.EmailService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/email")
@@ -33,5 +21,4 @@ public class EmailController {
 //
 //        return ResponseEntity.created(uri).body(email);
 //    }
-
 }

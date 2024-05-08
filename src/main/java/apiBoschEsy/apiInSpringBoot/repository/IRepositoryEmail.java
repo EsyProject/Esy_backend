dc531f9a7c60f40ac9966138899b14f0d615b33d
BrnGomes0 <brunownog05@gmail.com>
2024-05-08 15:40:21 -0300

    :hammer: fix: refactor the code

--- src/main/java/apiBoschEsy/apiInSpringBoot/repository/IRepositoryEmail.java
+++ src/main/java/apiBoschEsy/apiInSpringBoot/repository/IRepositoryEmail.java
@@ -2,6 +2,8 @@ package apiBoschEsy.apiInSpringBoot.repository;
 
 import apiBoschEsy.apiInSpringBoot.entity.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IRepositoryEmail extends JpaRepository<Email, Long> {
 }

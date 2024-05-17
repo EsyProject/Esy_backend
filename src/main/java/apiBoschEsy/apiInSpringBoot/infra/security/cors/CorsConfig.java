899f35717e300ad82e35b48b5574ff2108c3a603
BrnGomes0 <brunownog05@gmail.com>
2024-05-17 10:03:04 -0300

    :hammer: fix: fix the cors config (https replace for http)

--- src/main/java/apiBoschEsy/apiInSpringBoot/infra/security/cors/CorsConfig.java
+++ src/main/java/apiBoschEsy/apiInSpringBoot/infra/security/cors/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
-                .allowedOrigins("https://localhost:5173")
-                .allowedMethods("POST", "GET", "PATCH");
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PATCH");
     }
 }

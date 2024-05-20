0d96375b62fdc0a7981e43578fa5e3f7f859b0e2
BrnGomes0 <brunownog05@gmail.com>
2024-05-20 16:06:21 -0300

    :rewind: refactor: rewind the code

--- src/main/java/apiBoschEsy/apiInSpringBoot/ApiInSpringBootApplication.java
+++ src/main/java/apiBoschEsy/apiInSpringBoot/ApiInSpringBootApplication.java
@@ -2,10 +2,14 @@ package apiBoschEsy.apiInSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiInSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiInSpringBootApplication.class, args);
 	}
+
 }

fd852075cbc7f0b15c03e4400c764579013b15fc
BrnGomes0 <brunownog05@gmail.com>
2024-05-20 16:15:01 -0300

    :hammer: fix: fix the service

--- src/main/java/apiBoschEsy/apiInSpringBoot/service/email/EmailDefault.java
+++ src/main/java/apiBoschEsy/apiInSpringBoot/service/email/EmailDefault.java
@@ -12,21 +12,18 @@ public class EmailDefault {
     private FormatService formatService;
 
     // Creating a method for send e-mail with image in your body
-    public String emailDefault (String nameOfEvent, String imageUrl, DataAuth user){
-        if(formatService.isMorning()){
-            return "Prezado " + user.userName() + "," +
-                    "Bom dia! É com grande satisfação que lhe damos as boas-vindas ao " + nameOfEvent + "Agradecemos por fazer parte da nossa história e por estar presente conosco." +
-                    "Gostaríamos de fornecer a você uma imagem para autenticar o seu ticket na hora do evento. Certifique-se de tê-la em mãos para facilitar o processo de entrada." +
-                    imageUrl +
-                    "Aproveite ao máximo o evento e desfrute de todas as experiências que preparamos para você.\n" +
-                    "Atenciosamente, Esy Bosch";
-        }else {
-            return "Prezado " + user.userName() + "," +
-                    "Boa tarde! É com grande satisfação que lhe damos as boas-vindas ao " + nameOfEvent + "Agradecemos por fazer parte da nossa história e por estar presente conosco." +
-                    "Gostaríamos de fornecer a você uma imagem para autenticar o seu ticket na hora do evento. Certifique-se de tê-la em mãos para facilitar o processo de entrada." +
-                    imageUrl +
-                    "Aproveite ao máximo o evento e desfrute de todas as experiências que preparamos para você.\n" +
-                    "Atenciosamente, Esy Bosch";
-        }
+    public String emailDefault (String nameOfEvent, String cid, DataAuth user){
+        String greeting = formatService.isMorning() ? "Bom dia" : "Boa tarde";
+        return String.format(
+                "<p>Prezado %s,</p>" +
+                        "<p>%s! É com grande satisfação que lhe damos as boas-vindas ao %s. " +
+                        "Agradecemos por fazer parte da nossa história e por estar presente conosco.</p>" +
+                        "<p>Gostaríamos de fornecer a você uma imagem para autenticar o seu ticket na hora do evento. " +
+                        "Certifique-se de tê-la em mãos para facilitar o processo de entrada.</p>" +
+                        "<img src='cid:%s' alt='QR Code'>" +
+                        "<p>Aproveite ao máximo o evento e desfrute de todas as experiências que preparamos para você.</p>" +
+                        "<p>Atenciosamente, Esy Bosch</p>",
+                user.userName(), greeting, nameOfEvent, cid
+        );
     };
 }

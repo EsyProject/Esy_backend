8209830460e009ea9e6004a3c2034f61283af80c
BrnGomes0 <brunownog05@gmail.com>
2024-05-21 16:02:01 -0300

    :sparkles: feat: creating a validation in Repository

--- src/main/java/apiBoschEsy/apiInSpringBoot/repository/IRepositoryEvent.java
+++ src/main/java/apiBoschEsy/apiInSpringBoot/repository/IRepositoryEvent.java
@@ -8,8 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IRepositoryEvent extends JpaRepository<Event, Long> {
     Page<Event> findAll(Pageable pageable);
+    Optional<Event> findEventByNameOfEvent(String nameOfEvent);
 }

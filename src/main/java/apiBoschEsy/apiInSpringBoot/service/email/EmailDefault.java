a5d5d4a7ec887e03f8a5803e1700bd3cf728ae91
BrnGomes0 <brunownog05@gmail.com>
2024-05-21 16:05:46 -0300

    :hammer: fix: fix the email sending (Tamplate)

--- src/main/java/apiBoschEsy/apiInSpringBoot/service/email/EmailDefault.java
+++ src/main/java/apiBoschEsy/apiInSpringBoot/service/email/EmailDefault.java
@@ -12,18 +12,19 @@ public class EmailDefault {
     private FormatService formatService;
 
     // Creating a method for send e-mail with image in your body
-    public String emailDefault (String nameOfEvent, String cid, DataAuth user){
+    public String emailDefault (String nameOfEvent, DataAuth user){
         String greeting = formatService.isMorning() ? "Bom dia" : "Boa tarde";
         return String.format(
-                "<p>Prezado %s,</p>" +
-                        "<p>%s! É com grande satisfação que lhe damos as boas-vindas ao %s. " +
-                        "Agradecemos por fazer parte da nossa história e por estar presente conosco.</p>" +
-                        "<p>Gostaríamos de fornecer a você uma imagem para autenticar o seu ticket na hora do evento. " +
-                        "Certifique-se de tê-la em mãos para facilitar o processo de entrada.</p>" +
-                        "<img src='cid:%s' alt='QR Code'>" +
-                        "<p>Aproveite ao máximo o evento e desfrute de todas as experiências que preparamos para você.</p>" +
-                        "<p>Atenciosamente, Esy Bosch</p>",
-                user.userName(), greeting, nameOfEvent, cid
+                "Prezado %s,\n" +
+                        "É com grande satisfação que lhe damos as boas-vindas ao %s. Agradecemos por fazer parte da nossa história e por estar presente conosco.\n" +
+                        "Gostaríamos de fornecer a você uma imagem para autenticar o seu ticket na hora do evento. Certifique-se de tê-la em mãos para facilitar o processo de entrada.\n" +
+                        "Aproveite ao máximo o evento e desfrute de todas as experiências que preparamos para você.\n" +
+                        "Atenciosamente,\n" +
+                        "Esy Bosch",
+                user.userName(), greeting, nameOfEvent
         );
     };
+    public String titleEmail(String nameOfEvent){
+        return "Parabéns por particpar do evento (" + nameOfEvent + ")! Acesse o ticket!";
+    }
 }
